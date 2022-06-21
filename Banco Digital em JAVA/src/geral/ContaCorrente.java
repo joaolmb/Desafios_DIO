d753c0b33c3924d051a20ae4ec262130b765e97e
joaolmb <joaolmb2@gmail.com>
2022-06-20 21:54:26 -0300

    Projeto básico desenvolvido

---
+++ Banco Digital em JAVA/src/geral/ContaCorrente.java
@@ -0,0 +1,16 @@
+package geral;
+
+public class ContaCorrente extends Conta {
+
+	public ContaCorrente(Cliente cliente) {
+		super(cliente);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void imprimirExtrato() {
+		System.out.println("=== Extrato da Conta Corrente ===");
+		super.imprimirInfosComuns();
+	}
+	
+}
---
+++ Banco Digital em JAVA/src/geral/Main.java
@@ -0,0 +1,19 @@
+package geral;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente cliente = new Cliente();
+		cliente.setNome("João");
+		
+		Conta cc = new ContaCorrente(cliente);
+		Conta poupanca = new ContaPoupanca(cliente);
+		
+		cc.depositar(100);
+		cc.transferir(50, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+
+}
